d90b0750ef9797a8268c9eb890cad7e530713236
Administrator <Administrator@WIN-218RNDA7Q5I>
2013-06-15 13:20:49 +0800

    2013/06/15-13:20

--- FilesystemTest.py
+++ FilesystemTest.py
@@ -44,7 +44,7 @@ options:
                 if opt == '-f':
                     self.FSTYPE = value     
         except Exception,e:
-            print e
+            print str(e)
             self.Usage()
     
     def RunShell(self,cmd):
@@ -59,32 +59,57 @@ options:
         if fstype== '':
             fstype = self.FSTYPE
         cmd = "%s mkfs -t %s %s %s" %(other,fstype,extar,self.DEVICE)
-        self.RunShell(cmd)
+        if self.RunShell(cmd) !=0:
+            raise TestError('%s 创建文件系统失败' % fstype) 
     
     def MountFs(self,extar = ''):
         cmd = 'mount %s %s %s ' %(extar,self.DEVICE,self.mpoint)
-        self.RunShell(cmd)
+        if self.RunShell(cmd) != 0:
+            raise TestError("挂在%s分区失败" % self.DEVICE)
         
     def IsMount(self):
         try:
             mount_output = subprocess.Popen(["mount"],stdout=subprocess.PIPE)
             for line in mount_output.stdout.readlines():
-                if line.find(self.DEVICE) <> -1:
+                if line.find(self.DEVICE) != -1:
                     return True
                 else:
                     return False
         except Exception,e:
             print str(e)
             raise 
-    def Test(self):
-        pass
+        
+    def ChineseTest(self):
+        chinesepath=os.path.join(self.mpoint,'中文dir')
+        chinesefile=os.path.join(self.mpoint,'中文file')
+        try:
+        #创建中文目录
+            os.mkdir(chinesepath)
+        #创建中文文件
+            os.mkdir(chinesefile)
+        except Exception,e:
+            print str(e)
+            raise TestError("不支持中文")
+        if not os.path.isdir(chinesepath):
+            raise TestError("创建中文目录失败")
+        if not os.path.isfile(chinesefile):
+            raise TestError("创建中文文件失败")
+             
+        try:
+            fp = open(chinesefile,'r+')
+            fp.write("中文汉字 ")
+            fp.close()
+        except IOError,e:
+            print str(e)
+            raise TestError("写入中文出错")
+        
         
     def UnMount(self):
         try:
             cmd = 'unmount %s' % self.mpoint
         except Exception,e:
             print str(e)
-            sys.exit()
+            raise
             
     def run(self,fstype):
         try:
@@ -95,14 +120,15 @@ options:
             print "创建%s文件系统成功" % fstype
             self.MountFs()
             print "挂载%s分区到%s成功" %(self.DEVICE,self.mpoint)
-            self.Test()
+            if self.IsMount() == False:
+                raise TestError("%s挂在失败" % fstype)
+            self.ChineseTest()
             print "%s编码测试PASS" % fstype
             self.UnMount()
-            if not self.IsMount():
-                print "卸载%s分区成功" % self.DEVICE
-            else: 
-                print "卸载%s分区失败" % self.DEVICE
             print "%s TEST PASS" % fstype
+        except TestError,e:
+            print str(e)
+            print "%s TEST FAIL" % fstype
         except Exception,e:
             #print str(e)
             print "%s TEST FAIL" % fstype
@@ -120,6 +146,13 @@ options:
                 self.run(fstype)
         else:
             self.run(self.FSTYPE)
+        
+class TestError(Exception):
+    def __init__(self,output):
+        self.output = output
+    
+    def __str__(self):
+        print self.output
         
 if __name__ == '__main__':
     ft = FsTest(DEVICE,FSTYPE)
