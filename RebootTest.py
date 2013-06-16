151731a34e638645de7f9139feaf47267bef4090
Administrator <Administrator@WIN-218RNDA7Q5I>
2013-06-16 22:02:30 +0800

    20130616-22:02

--- RebootTest.py
+++ RebootTest.py
@@ -10,13 +10,13 @@ import ConfigParser
 
 numberfile='/tmp/number.txt'
 TIMES = 50 
-INTERVAL = 180
+INTERVAL = 300
 
 class ReBoot:
     def __init__(self,numberfile,TIMES,INTERVAL):
         self.file = numberfile
         self.TIMES = TIMES
-        self.TIME = None
+        self.OTIME = 0
         self.FAIL = 0
         self.TRUE = 0
         self.INTERVAL = INTERVAL
@@ -33,21 +33,21 @@ class ReBoot:
     def FileWrite(self):
         self.cf.add_section("REBOOT")
         self.cf.set("REBOOT", "TIMES", self.TIMES)
-        self.cf.set("REBOOT", "TIME",self.TIME)
+        self.cf.set("REBOOT", "TIME",self.OTIME)
         self.cf.set("REBOOT", "FAIL",self.FAIL)
         self.cf.set("REBOOT", "TRUE",self.TRUE)
-        self.cf.write(self.file)
+        self.cf.write(open(self.file,'w'))
     
     def FileRead(self):
         self.cf.read(self.file)
         self.TIMES = self.cf.getint("REBOOT", "TIMES")
-        self.TIME =  self.cf.getint("REBOOT", "TIME")
+        self.OTIME =  self.cf.getfloat("REBOOT", "TIME")
         self.FAIL = self.cf.getint("REBOOT", "FAIL")
         self.TRUE = self.cf.getint("REBOOT", "TRUE")
     #读写文件方法定义
     def ParserInit(self):
         self.cf=ConfigParser.ConfigParser()
-        self.cf.read(self.file)
+        #self.cf.read(self.file)
         
     def TimeStamp(self):
         return time.mktime(time.localtime())
@@ -56,26 +56,29 @@ class ReBoot:
         self.FileExists()
         if self.TIMES ==0:
             sys.exit()
-        if self.cf.get("REBOOT", self.TIME) == None:
+        if self.OTIME == 0:
             self.cf.set("REBOOT", "TIME", self.TimeStamp())
-            self.cf.write(self.file)
-            os.system("uptime")
-        nowtime = self.TimeStamp()
-        diffence = float(nowtime) - float(self.TIME)
-        if diffence > self.INTERVAL:
-            self.FAIL = self.FAIL + 1
-            self.TIMES = self.TIMES - 1
+            self.cf.write(open(self.file),'w')
+            os.system("reboot")
         else:
-            self.TRUE = self.TRUE + 1
-        self.TIMES = int(self.TIMES) - 1
-        self.cf.set("REBOOT", "TIMES", self.TIME)
-        self.cf.set("REBOOT", "TIME", self.TimeStamp())
-        self.cf.set("REBOOT", "FAIL",self.FAIL)
-        self.cf.set("REBOOT", "TRUE",self.TRUE)
-        self.cf.write(self.file)
-        os.system('uptime')
+            nowtime = self.TimeStamp()
+            diffence = float(nowtime) - self.OTIME
+            if diffence >= self.INTERVAL:
+                self.FAIL = self.FAIL + 1
+                self.TIMES = self.TIMES - 1
+                self.TRUE = self.TRUE + 1
+            else:
+                self.TRUE = self.TRUE + 1
+            self.TIMES = self.TIMES - 1
+            self.cf.set("REBOOT", "TIMES", self.TIMES)
+            self.cf.set("REBOOT", "TIME", self.TimeStamp())
+            self.cf.set("REBOOT", "FAIL",self.FAIL)
+            self.cf.set("REBOOT", "TRUE",self.TRUE)
+            self.cf.write(open(self.file,'w'))
+            os.system('reboot')
         
 
 if __name__=="__main__":
+    #time.sleep(60)
     R = ReBoot(numberfile,TIMES,INTERVAL)
     R.run()
